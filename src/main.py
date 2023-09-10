21e24b13a5be6de5c7da3f8172a162b5d63cbc97
MinyMinz <minhaz_rahman543@outlook.com>
2023-09-10 22:10:06 +0100

    Update main.py for swagger docs
    Format swagger docs

--- src/main.py
+++ src/main.py
@@ -5,7 +5,56 @@ from routes.holidayRequests import holidayRouter
 from routes.team import teamRouter
 from routes.role import roleRouter
 
-app = FastAPI(title="Phoebus Software Shared Calendar API")
+description = """
+The Team Holiday Manager API is a RESTful API that allows you to manage your team's holiday requests. 
+
+## Roles
+You can:
+* **Create Roles**.
+* **Read Roles**.
+
+## Teams
+You can:
+* **Create Teams**.
+* **Read Teams**.
+
+## Users
+You can:
+* **Create Users**.
+* **Read Users**.
+* **Update Users**.
+* **Delete Users**.
+
+## Holiday Requests
+You can:
+* **Create Holiday Requests**.
+* **Read Holiday Requests**.
+* **Update Holiday Requests**.
+* **Delete Holiday Requests**.
+"""
+
+tags_metadata = [
+    {
+        "name": "Roles",
+        "description": "Operations with Roles.",
+    },
+    {
+        "name": "Teams",
+        "description": "Operations with Teams.",
+    },
+    {
+        "name": "Users",
+        "description": "Operations with users.",
+    },
+    {
+        "name": "Holiday Requests",
+        "description": "Operations with Holiday Requests.",
+    }
+]
+
+
+
+app = FastAPI(title="Phoebus Software Shared Calendar API", openapi_tags=tags_metadata, description=description)
 
 # Set up CORS
 origins = ["*"]  # Allow requests from any origin
@@ -18,10 +67,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(roleRouter, prefix="/roles")
-app.include_router(teamRouter, prefix="/teams")
-app.include_router(userRouter, prefix="/users")
-app.include_router(holidayRouter, prefix="/holiday-request")
+app.include_router(roleRouter, prefix="/roles", tags=["Roles"])
+app.include_router(teamRouter, prefix="/teams", tags=["Teams"])
+app.include_router(userRouter, prefix="/users", tags=["Users"])
+app.include_router(holidayRouter, prefix="/holiday-request", tags=["Holiday Requests"])
 
 import uvicorn
 
